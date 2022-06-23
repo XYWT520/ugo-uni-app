aa39448254e413c5f7ffbe9d10c699f4cd880012
ZYC <2095921712@qq.com>
2022-06-23 10:07:09 +0800

    feat:分类完成

--- utils/http.js
+++ utils/http.js
@@ -1,6 +1,6 @@
 import { $http } from '@escook/request-miniprogram'
 // 配置请求根路径
-$http.baseUrl = "https://api-ugo-web.itheima.net"
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
 
 // 将 http 挂载到全局的对象上
 uni.$http = $http
